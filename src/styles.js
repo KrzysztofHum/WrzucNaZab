d1388d44637a6cc7275c53650050f2e62907c9c3
KrzysztofHum <k.humienny1996@protonmail.com>
2021-05-16 11:51:18 +0200

    ChooseScreenAndOrderType

--- src/styles.js
+++ src/styles.js
@@ -1,32 +1,49 @@
-import {makeStyles} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
-	root: {
-		height: '100vh',
-		display: 'flex',
-		flexDirection: 'column',
-	},
-	orange: {
-		backgroundColor: '#fea735',
-		color: '#ffffff'
-	},
-	main: {
-		flex: 1,
-		overflow: 'auto',
-		flexDirection: 'column',
-		display: 'flex',
-		color: '#ffffff',
-	},
-	center: {
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		textAlign: 'center',
-	},
-	blue: {
-		backgroundColor: '#00c3ff',
-	},
-	largeLogo: {
-		height: 100,
-	},
-}))
+  root: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+  },
+  navy: {
+    backgroundColor: "#00c3ff",
+  },
+  orange: {
+    backgroundColor: "#fea735",
+    color: "#ffffff",
+  },
+  main: {
+    flex: 1,
+    overflow: "auto",
+    flexDirection: "column",
+    display: "flex",
+    color: "#ffffff",
+  },
+  center: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  blue: {
+    backgroundColor: "#00c3ff",
+  },
+  largeLogo: {
+    height: 100,
+  },
+  cards: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    margin: 10,
+  },
+  space: {
+    padding: 10,
+  },
+  media: {
+    width: 200,
+  },
+}));
